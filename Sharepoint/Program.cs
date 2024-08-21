b06dd3ad0da4572cf26a7884f493fbf3a57858c3
viveksimform <vivek.u@simformsolutions.com>
2024-08-21 18:22:28 +0530

    Initial Commit

---
+++ Sharepoint/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Sharepoint.Services;
+
+
+var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+var sharePointSettings = configuration.GetSection("SharePointSettings");
+string clientId = sharePointSettings["ClientId"];
+string tenantId = sharePointSettings["TenantId"];
+string clientSecret = sharePointSettings["ClientSecret"];
+string sharePointUrl = sharePointSettings["SharePointUrl"];
+string folderName = sharePointSettings["FolderName"];
+string fileName = sharePointSettings["FileName"];
+
+
+SharePointService sharePointService = new SharePointService(clientId, tenantId, clientSecret);
+var accessToken = await sharePointService.GetAccessTokenAsync();
+var data = await sharePointService.GetSharePointSiteDataAsync(sharePointUrl);
+Console.WriteLine(JsonConvert.SerializeObject(data));
+var data1 = await sharePointService.GetListItemsAsync(sharePointUrl, folderName);
+await sharePointService.UploadFileAsync(sharePointUrl, folderName, fileName);
+Console.Read();
---
+++ Sharepoint/Services/SharePointService.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Identity.Client;
+using System.Net.Http.Headers;
+
+namespace Sharepoint.Services
+{
+    public class SharePointService
+    {
+        private readonly string clientId;
+        private readonly string tenantId;
+        private readonly string clientSecret;
+        private readonly string authority;
+        private readonly string scope;
+
+        public SharePointService(string clientId, string tenantId, string clientSecret)
+        {
+            this.clientId = clientId;
+            this.tenantId = tenantId;
+            this.clientSecret = clientSecret;
+            this.authority = $"https://login.microsoftonline.com/{tenantId}";
+            this.scope = "https://graph.microsoft.com/.default"; // or SharePoint scope
+        }
+
+        public async Task<string> GetAccessTokenAsync()
+        {
+            var app = ConfidentialClientApplicationBuilder.Create(clientId)
+                .WithClientSecret(clientSecret)
+                .WithAuthority(new Uri(authority))
+                .Build();
+
+            var result = await app.AcquireTokenForClient(new[] { scope }).ExecuteAsync();
+            return result.AccessToken;
+        }
+        public async Task<string> GetSharePointSiteDataAsync(string siteUrl)
+        {
+            var accessToken = await GetAccessTokenAsync();
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                var response = await client.GetAsync($"{siteUrl}/_api/web");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    throw new HttpRequestException("Failed to fetch data from SharePoint");
+                }
+            }
+        }
+        public async Task<string> GetListItemsAsync(string siteUrl, string listName)
+        {
+            var accessToken = await GetAccessTokenAsync();
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                var response = await client.GetAsync($"{siteUrl}/_api/web/lists/getbytitle('{listName}')/items");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    throw new HttpRequestException("Failed to fetch list items from SharePoint");
+                }
+            }
+        }
+        public async Task UploadFileAsync(string siteUrl, string libraryName, string filePath)
+        {
+            var accessToken = await GetAccessTokenAsync();
+            var fileBytes = System.IO.File.ReadAllBytes(filePath);
+            var fileName = System.IO.Path.GetFileName(filePath);
+            var uploadUrl = $"{siteUrl}/_api/web/GetFolderByServerRelativeUrl('{libraryName}')/Files/add(url='{fileName}',overwrite=true)";
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                var content = new ByteArrayContent(fileBytes);
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                var response = await client.PostAsync(uploadUrl, content);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new HttpRequestException("Failed to upload file to SharePoint");
+                }
+            }
+        }
+    }
+}
